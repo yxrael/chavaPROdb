5b264a85dfbd95ba6baf51eff79e65255436bd0d
yxrael <yxrael@gmail.com>
2022-03-04 20:02:16 +0100

    responsive en listado admin

--- src/components/panelAdmin/ProductoStockAdmin.jsx
+++ src/components/panelAdmin/ProductoStockAdmin.jsx
@@ -120,100 +120,76 @@ const ProductoStockAdmin = ( {producto, setNuevoItem, modoEdicion, setModoEdicio
                     <div className='row'>
 
                         <div className='col-1 bg-warning fas fa-edit text-light d-flex align-items-center justify-content-center'>
-
                         </div>
 
                             <div className='col-10'>
 
-                                <div className="d-flex justify-content-between">
-                                    <div className="mt-2">
-                                        <div className="">
-                                            <div className="">
-                                                <p><strong>{pais}. {nombre}</strong></p>
-                                            </div>
-                                                
-                                            <div className="">
-                                                <p>Precio: <b>{precio}€/kg</b></p>
-                                            </div>
-                                                
-                                        </div>    
-                                    </div>
-
-                                    <div className="mt-2">
-                                        <div className="">
-                                            <p><b>{continente}</b></p>       
-                                        </div>
-                                        <div>
-                                            <p>{proceso}</p>
-                                        </div>
-                                    </div>
-
-                                    <div className="mt-2">
-                                        
-                                                <p>Disponible: <strong>
-                                                    {
-                                                    disponible
-                                                    ? (
-                                                        <span className='text-success'>SI </span>   
-                                                    )
-                                                    :
-                                                    (
-                                                        <span className='text-danger'>NO </span>
-                                                    )
-                                                    }    
-
-                                                </strong></p>
-                                                <button 
-                                                    className='btn btn-info btn-sm m-2 d-block'
-                                                    onClick={ handleCambioDispo }
-                                                    >
-                                                        Dispo: SI/NO
-                                                </button>
-                                                {/* <div className="d-flex justify-content-center">
-                                                    <button 
-                                                        className='btn btn-danger btn-sm m-2 d-block'
-                                                        onClick={ handleEliminar }
-                                                        >
-                                                            X
-                                                    </button>
-                                                </div>
-                                                <div className="d-flex justify-content-center">
-                                                    <button 
-                                                        className='btn btn-warning btn-sm m-2 d-block'
-                                                        onClick={ handleEditar }
-                                                        >
-                                                            Editar
-                                                    </button>
-                                                </div> */}
-
-                                    </div>
-                                        
-                                        {/* <div className="mt-2">
-                                            
-                                            <div className="mb-2">
-                                                    <input
-                                                        name={id}
-                                                        type="number"
-                                                        className="form-control"
-                                                        min="0"
-                                                        max="50"
-                                                        placeholder="Cantidad de kgs:"
-                                                        value={cantidadElegida}
-                                                        onChange={ handleCambioCantidad }
-                                                    />
-                                            </div>
-                                            <p>Total: <b>{precioPorProducto}€/kg</b></p>
-                                        </div> */}
-                                </div>   
+                                <div className="d-flex justify-content-between align-items-start">
                                     
-                                <div className="text-center">
+                                    <div>
+                                        <div className="mt-2">
+                                            <div className="">
+                                                <div className="">
+                                                    <p><strong>{pais}. {nombre}</strong></p>
+                                                </div>
+                                                    
+                                                <div className="">
+                                                    <p>Precio: <b>{precio}€/kg</b></p>
+                                                </div>
+                                                    
+                                            </div>    
+                                        </div>
+
+
+                                       
+                                            
+                                        </div>
+                                            <div className="mt-2">
+                                                <div className="">
+                                                    <p><b>{continente}</b></p>       
+                                                </div>
+                                                <div>
+                                                    <p>{proceso}</p>
+                                                </div>
+                                            </div>
+                                        
+                                </div>  
+
+                                <div className="d-flex justify-content-center">
                                     <span className='badge rounded-pill bg-warning  text-dark m-2'>{infoExtra}</span>
+                                </div> 
+                                    
+                                                
+                                <div className=" d-flex align-items-center justify-content-between">
+                                            
+                                    <p className='m-2'>Disponible: <strong>
+                                        {
+                                        disponible
+                                        ? (
+                                            <span className='text-success m-2'>SI </span>   
+                                        )
+                                        :
+                                        (
+                                            <span className='text-danger m-2'>NO </span>
+                                        )
+                                        }    
+
+                                    </strong></p>
+                                    <button 
+                                        className='btn btn-info btn-sm m-2 d-block'
+                                        onClick={ handleCambioDispo }
+                                        >
+                                            Dispo: SI/NO
+                                    </button>
+
                                 </div>
+
+
+
                                     
                             </div>
 
                         <div className='col-1 bg-danger fas fa-trash-alt text-light d-flex align-items-center justify-content-center'>    
-
                         </div>
 
                     </div>
