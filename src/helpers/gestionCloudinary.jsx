5d8581ec088f388b3d715816b359c1d9acf01f4a
yx_fatima <yxrael@gmail.com>
2022-03-22 13:39:01 +0100

    dealing with cloudinary delete

--- src/helpers/gestionCloudinary.jsx
+++ src/helpers/gestionCloudinary.jsx
@@ -31,10 +31,10 @@ export const subeImagenDetalles = async ( rutaFile ) => {
 
 export const borraImagenDetalles = async ( idImagen ) => {
     
-    const cloudURL = 'https://api.cloudinary.com/v1_1/dtm3z7jee/destroy';
+    const cloudURL = 'https://api.cloudinary.com/v1_1/dtm3z7jee/image/destroy';
 
     const formData = new FormData();
-    formData.append('upload_preset','chava-pro');
+    // formData.append('upload_preset','chava-pro');
     formData.append('public_id', idImagen );
 
     try {
